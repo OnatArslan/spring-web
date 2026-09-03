1bd7b56e320e88c0637224c99220e120cc053df4
OnatArslan <onatarslan13@gmail.com>
2026-09-04 02:48:02 +0300

    initial commit

---
+++ src/main/java/com/onatarslan/orbitweb/OrbitWebApplication.java
@@ -0,0 +1,13 @@
+package com.onatarslan.orbitweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbitWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onatarslan/orbitweb/OrbitWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onatarslan.orbitweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
